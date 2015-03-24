0abe2ccf22642f9b27fb7308f6ff66ff7ab98bb5
Kameron Johnson <KameronJohnson@gmail.com>
2015-03-24 12:31:54 -0700

    Allow user to add a new question.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,23 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:notice] = "Question successfully added."
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
+  end
+
+private
+  def question_params
+    params.require.(:question).permit(:title, :query)
+  end
 end
