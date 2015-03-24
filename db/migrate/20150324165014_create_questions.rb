b8d853907a110956adb02febee68d9fcc89ef60b
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-03-24 10:24:19 -0700

    Initialize files and folders.

---
+++ db/migrate/20150324165014_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+
+      t.column :title, :string
+      t.column :query, :string
+      t.column :user_id, :integer
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory(:question) do
+    name('example question')
+  end
+end
