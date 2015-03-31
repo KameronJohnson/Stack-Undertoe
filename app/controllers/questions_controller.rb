097b564dd34a1fa626aa77f5e7f6186cee15a0b8
Heather <heather@thisisnotanorg.org>
2015-03-31 10:15:54 -0700

    Added AJAX functionality to new question form.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,21 +5,30 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
+    @question = Question.new(params[:question])
   end
 
   def create
     @question = Question.new(question_params)
-    if @question.save
+    @question.save
       flash[:notice] = "Question successfully added."
-      redirect_to question_path(@question)
-    else
-      render :new
+          respond_to do |format|
+      format.html { redirect_to questions_path }
+      format.js
+      # redirect_to question_path(@question)
     end
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def show
+    @question = Question.find(params[:id])
   end
 
 private
   def question_params
-    params.require.(:question).permit(:title, :query)
+    params.require(:question).permit(:title, :query)
   end
 end
